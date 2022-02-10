574212ddba446cd8bf8cbae8bb35b9e3bbb61210
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-09 16:20:18 -0800

    details page rendering

--- src/DetailPage.js
+++ src/DetailPage.js
@@ -4,8 +4,17 @@ import { getGameById } from './services/fetch-utils';
 
 export default function DetailPage() {
   const [game, setGame] = useState({});
+  let match = useRouteMatch('/board-games/:id');
 
   // on mount, fetch and set in state the correct board game for this id (the id can be found in match.params using the correct react-router hook)
+  useEffect(() => {
+    async function fetch() {
+      const thisGame = await getGameById(match.params.id);
+
+      setGame(thisGame);
+    }
+    fetch();
+  }, []);
 
   return (
     <div className='detail'>
